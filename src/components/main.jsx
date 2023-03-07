ec9478e4ef3702bcb6b0a222cd88ddb20c3613d5
PozolitoVerde <m69e10z21a@gmail.com>
2023-03-06 20:54:12 -0600

    Frontend Login

---
+++ src/components/main.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Contants from 'expo-constants'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+
+const Main = () => {
+    return(
+        <View style={styles.container}>
+            <Image
+            source={require("../../img/cultivecare.png")}
+            style={styles.img}
+            />
+            <Text style={styles.h1}>Iniciar Sesión</Text>
+            
+            <Text style={styles.h2}>Por favor, ingrese sus datos para acceder</Text>
+            <View style={styles.containerIn}>
+                <Text style={styles.p}>Email</Text>
+                <TextInput
+                    style={styles.Ipt}                
+                    placeholder='Ingresa tu correo'
+                />
+                <Text style={styles.p}>Password</Text>
+                <TextInput
+                    style={styles.Ipt}
+                    placeholder='Ingresa tu contraseña'
+                />
+            </View>
+            <TouchableOpacity
+                style={styles.Btn}
+                >
+                    <Text style={styles.Btntxt}>Entrar</Text>
+                </TouchableOpacity>
+
+            <View
+            style={styles.containerDown}
+            >
+                <Text
+                    style={styles.h2}
+                >
+                    Olvidaste tu contraseña?
+                </Text>
+                <TouchableOpacity>
+                    <Text
+                        style={styles.registrate}
+                    >
+                        Registrarse
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+export default Main;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, 
+    containerIn:{
+        width: '80%',
+        
+    },
+    containerDown:{
+        
+        flexDirection: 'row',
+        width: '80%',
+        marginTop: 40 
+    
+    },
+    h1: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    h2: {
+        fontSize: 17,
+        color: 'grey'
+    },
+    p:{
+        color: 'green',
+        alignSelf: 'baseline',
+        marginTop: 30,
+        marginBottom: 5
+    },
+    registrate: {
+        color: 'green',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingLeft: 40
+    },
+    Ipt:{
+        padding: 10,
+        width: '100%',
+        paddingStart: 0
+    },
+    Btn:{
+        width: '80%',
+        backgroundColor: 'green',
+        alignItems: 'center',
+        padding: 15,
+        borderRadius: 5,
+        marginTop: 25
+    },
+    Btntxt:{
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 18
+
+    },
+    img:{
+        width: '16rem',
+        height: '10rem',
+        marginBottom: 30
+    }
+
+    
+
+    
+})
